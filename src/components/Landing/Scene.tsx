d13e9ea7d20802c3496989162a4d697c31ac936e
saur0w <sthapliyal085@gmail.com>
2026-07-07 07:51:56 +0530

    feat: add 3D scene component with React Three Fiber canvas

--- src/components/Landing/Scene.tsx
+++ src/components/Landing/Scene.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import Mesh from "./Mesh";
 import styles from "./scene.module.scss";
 
 export default function Scene() {
-    return (
-        <div className={styles.scene}>
-            <Canvas
-                camera={{ position: [0, 0, 3], fov: 60 }}
-                gl={{ antialias: true, alpha: true }}
-            >
-                <ambientLight intensity={1.5} />
-                <Mesh />
-            </Canvas>
-        </div>
-    )
+  return (
+    <div className={styles.scene}>
+      <Canvas camera={{ position: [0, 0, 3], fov: 60 }} gl={{ antialias: true, alpha: true }}>
+        <ambientLight intensity={1.5} />
+        <Suspense fallback={null}>
+          <Mesh />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
