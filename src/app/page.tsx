c10cc8d5121d8b355c9f27db8d8fd7eafba1b55d
saur0w <sthapliyal085@gmail.com>
2026-07-09 17:01:53 +0530

    feat: add client directive and remove Preloader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import styles from "./page.module.css";
 import Landing from "@/components/Landing";
 import Header from "@/components/Header";
--- src/components/Preloader/index.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import styles from "./style.module.scss";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
